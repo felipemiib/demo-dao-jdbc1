1c361221f31e7fad26484bda240d6d6aaad41f4e
felipemiib <felipemiib@gmail.com>
2021-11-09 12:31:34 -0300

    Find Seller by DepartmentID

--- demo-dao-jdbc1/src/application/Program.java
+++ demo-dao-jdbc1/src/application/Program.java
@@ -1,7 +1,10 @@
 package application;
 
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
@@ -11,11 +14,25 @@ public class Program {
 		
 		// Seller seller = new Seller(21,"Bob","bob@gmail.com", new Date(), 3000.00, obj);
 		
-		SellerDao sellerdao = DaoFactory.createSellerDao();
+		
 		System.out.println("------------- Testes N1: seller findById -----------");
+		SellerDao sellerdao = DaoFactory.createSellerDao();
 		Seller seller = sellerdao.findById(3);
 		System.out.println(seller);
-				
+		
+		System.out.println("------------- Testes N2: seller findByDepartment -----------");
+		
+		Department department = new Department(2, null); //instancia um novo objeto Departamento (model entities) e informa os parametros
+		
+		List<Seller> list = sellerdao.findByDepartment(department);
+		
+		for (Seller obj : list) {
+			
+			System.out.println(obj);
+			
+		}
+		
+		
 		//System.out.println(obj);
 		
 	}
--- demo-dao-jdbc1/src/model/dao/implementation/SellerDaoJBDBC.java
+++ demo-dao-jdbc1/src/model/dao/implementation/SellerDaoJBDBC.java
@@ -4,7 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import db.DB;
 import db.DbException;
@@ -13,6 +16,7 @@ import model.entities.Department;
 import model.entities.Seller;
 
 public class SellerDaoJBDBC implements SellerDao {
+	
 	//conexão com o banco de dados JDBC MYSQL
 	private Connection conn;
 	
@@ -47,7 +51,7 @@ public class SellerDaoJBDBC implements SellerDao {
 					"SELECT seller.*,department.Name as DepName "
 					+ "FROM seller INNER JOIN department "
 					+ "ON seller.DepartmentId = department.Id "
-					+ "WHERE seller.Id = ?)");
+					+ "WHERE seller.Id = ?");
 			st.setInt(1, id);
 			rs = st.executeQuery();
 			if(rs.next()) {
@@ -55,6 +59,7 @@ public class SellerDaoJBDBC implements SellerDao {
 				Department dep = instantiateDepartment(rs); // foi criada essa função para deixar o codigo melhor estruturado
 				
 				Seller obj = instantiateSeller(rs, dep); // foi criada essa função para deixar o codigo melhor estruturado
+				return obj;
 			}
 			
 			return null;
@@ -69,12 +74,22 @@ public class SellerDaoJBDBC implements SellerDao {
 		}
 	}
 
+
+
+	private Department instantiateDepartment(ResultSet rs) throws SQLException {
+		Department dep = new Department();
+		dep.setId(rs.getInt("DepartmentId")); // tem que identico ao atributo criado no banco MYSQL
+		dep.setName(rs.getString("DepName"));
+		return dep;
+	}
+	
+	
 	private Seller instantiateSeller(ResultSet rs, Department dep) throws SQLException {
 		Seller obj = new Seller();  // criando um objeto Seller
 		obj.setId(rs.getInt("Id")); // tem que identico ao atributo criado no banco MYSQL
 		obj.setName(rs.getString("Name")); // tem que identico ao atributo criado no banco MYSQL
 		obj.setEmail(rs.getString("Email")); // tem que identico ao atributo criado no banco MYSQL
-		obj.setBithDate(rs.getDate("BithDay")); // tem que identico ao atributo criado no banco MYSQL
+		//obj.setBithDate(rs.getDate("BirthDate")); // tem que identico ao atributo criado no banco MYSQL
 		obj.setBaseSalary(rs.getDouble("BaseSalary")); // tem que identico ao atributo criado no banco MYSQL
 		obj.setDepartment(dep);	// não pode ser o Id do MYSQL, e sim a referencia de objeto na classe Seller.java
 		//aqui nesse scrip, como foi criado um novo objeto, deve ser incluído a variável que contempla, nesse caso "dep"
@@ -83,17 +98,61 @@ public class SellerDaoJBDBC implements SellerDao {
 	}
 
 
-	private Department instantiateDepartment(ResultSet rs) throws SQLException {
-		Department dep = new Department();
-		dep.setId(rs.getInt("DepartmentId")); // tem que identico ao atributo criado no banco MYSQL
-		dep.setName(rs.getString("DepName"));
-		return null;
-	}
 
 
 	@Override
 	public List<Seller> findAll() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+
+	@Override
+	public List<Seller> findByDepartment(Department department) {
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		
+		try {
+			st = conn.prepareStatement(
+					"SELECT seller.*,department.Name as DepName "
+					+ "FROM seller INNER JOIN department "
+					+ "ON seller.DepartmentId = department.Id "
+					+ "WHERE DepartmentId = ? "
+					+ "ORDER BY Name");
+			st.setInt(1, department.getId()); // O Id do departamento ja é informado no inicio da função "findByDepartment(Department department)"
+			
+			rs = st.executeQuery();
+			
+			List<Seller> list = new ArrayList<>();
+			
+			Map<Integer, Department> map = new HashMap();
+			
+			while (rs.next()) {
+				
+				Department dep = map.get(rs.getInt("DepartmentId")); // atribui o IdDepartment informado na estrutura map para verificar se existe o objeto department
+				//inicio da verificação se ja existe o objeto departamento com o id informado
+				
+				if (dep == null) {
+					dep = instantiateDepartment(rs);
+					map.put(rs.getInt("DepartmentId"), dep); //gravar na estrutura map 
+				}
+				
+				//fim da verificação se ja existe o objeto departamento com o id informado				
+				
+				Seller obj = instantiateSeller(rs, dep); // foi criada essa função para deixar o codigo melhor estruturado
+				
+				list.add(obj);
+			}
+			
+			return list;
+		}
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		
+		finally {
+			DB.closeStatement(st);;
+			DB.closeResultSet(rs);
+		}
 	}
 }
